11140acfa364f3238b4f132dfc686dddb9a094c4
endink <sharping@outlook.com>
2020-11-10 11:07:49 +0800

    add extension method

--- core-starter/src/main/kotlin/com/labijie/infra/telemetry/tracing/ExtensionMethods.kt
+++ core-starter/src/main/kotlin/com/labijie/infra/telemetry/tracing/ExtensionMethods.kt
@@ -1,9 +1,8 @@
 package com.labijie.infra.telemetry.tracing
 
-import com.labijie.infra.telemetry.tracing.propagation.MapGetter
-import com.labijie.infra.telemetry.tracing.propagation.MapSetter
 import io.grpc.Context
-import io.opentelemetry.OpenTelemetry
+import io.opentelemetry.context.ContextUtils
+import io.opentelemetry.context.Scope
 import io.opentelemetry.trace.Span
 import io.opentelemetry.trace.Tracer
 import io.opentelemetry.trace.TracingContextUtils
@@ -11,3 +10,19 @@ import io.opentelemetry.trace.TracingContextUtils
 fun Tracer.extractSpan(map: Map<String, Any>): Span = TracingManager.extractSpan(map)
 
 fun Tracer.injectSpan(map: MutableMap<String, Any>) = TracingManager.injectSpan(map)
+
+
+val Context.span: Span?
+    get(){
+        val span = TracingContextUtils.getSpanWithoutDefault(this)
+        return if(span != null && span.context.isValid){
+            span
+        }else{
+            null
+        }
+    }
+
+fun Context.startScope(span: Span): Scope {
+    val context = TracingContextUtils.withSpan(span, this)
+    return ContextUtils.withScopedContext(context)
+}
