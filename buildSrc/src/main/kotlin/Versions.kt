750fe479e1503897d9e9550c6d2a17a792c9e5ea
endink <sharping@outlook.com>
2021-12-13 01:09:29 +0800

    fix dependencies version declare

--- buildSrc/src/main/kotlin/Versions.kt
+++ buildSrc/src/main/kotlin/Versions.kt
@@ -8,7 +8,7 @@ object Versions {
     const val infraCommons = "2.2.1"
     const val kotlin = "1.6.0"
     const val infraBom = "2.6.3"
-    const val infraOrm = "1.0.0"
+    const val infraOrm = "1.0.1"
     const val infraPlugin = "1.0.7"
     const val gitProperties = "2.4.0-rc2"
     const val openTelemetry = "1.9.1"
